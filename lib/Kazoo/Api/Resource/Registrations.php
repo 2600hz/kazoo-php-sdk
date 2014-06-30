ce4e93e979f8fa8b539e947b3382554aa1ebe578
Jonathan Hopkins <jhopkins@whitelabelcomm.com>
2014-06-30 09:30:45 -0400

    Fix spelling error for Registrations

--- lib/Kazoo/Api/Resource/Registrations.php
+++ lib/Kazoo/Api/Resource/Registrations.php
@@ -7,13 +7,13 @@ use Kazoo\Api\AbstractResource;
  * 
  */
 class Registrations extends AbstractResource {
-    
-    protected static $_entity_class = "Kazoo\\Api\\Data\\Entity\\Regsitration";
-    protected static $_entity_collection_class = "Kazoo\\Api\\Data\\Collection\\RegsitrationCollection";
+
+    protected static $_entity_class = "Kazoo\\Api\\Data\\Entity\\Registration";
+    protected static $_entity_collection_class = "Kazoo\\Api\\Data\\Collection\\RegistrationCollection";
     protected static $_schema_name = "phone_numbers.json";
     
     public function __construct(\Kazoo\Client $client, $uri) {
         parent::__construct($client, $uri);
     }
-    
+
 }
