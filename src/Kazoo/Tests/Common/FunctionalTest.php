10d24d47f5b839161aaabeb3849fbc54a93c03e8
ricky ing <ricky@2600hz.com>
2014-09-09 19:53:09 -0700

    Unit Testing for CDRs, remove CDRs from todo list and tweaks for FunctionalTest.php & phpunit.xml

--- src/Kazoo/Tests/Common/FunctionalTest.php
+++ src/Kazoo/Tests/Common/FunctionalTest.php
@@ -7,8 +7,8 @@ use \PHPUnit_Framework_TestCase;
 use \Kazoo\SDK;
 
 use \Kazoo\AuthToken\ApiKey;
+use \Kazoo\AuthToken\User;
 use \Kazoo\AuthToken\Exception\Unauthenticated;
-
 use \Kazoo\Api\Exception\RateLimit;
 
 class FunctionalTest extends \PHPUnit_Framework_TestCase
@@ -17,11 +17,21 @@ class FunctionalTest extends \PHPUnit_Framework_TestCase
 
     public function setUp() {
         @session_start();
-        $api_key = $GLOBALS['api_key'];
-        $auth_token = new ApiKey($api_key);
+
+        if(empty($GLOBALS['api_key'])){
+            $auth_token = new User($GLOBALS['auth_username'], $GLOBALS['auth_password'], $GLOBALS['auth_realm']);
+        } else {
+            $auth_token = new ApiKey($GLOBALS['api_key']);
+        }
+        
+        if(empty($GLOBALS['base_url'])){
+            $options = array();
+        } else {
+            $options = array('base_url' => $GLOBALS['base_url']);
+        }
 
         try {
-            $this->sdk = new SDK($auth_token);
+            $this->sdk = new SDK($auth_token, $options);
         } catch (RateLimit $e) {
             $this->markTestSkipped('API limit reached. Skipping to prevent unnecessary failure.');
         } catch (Unauthenticated $e) {
---
+++ src/Kazoo/Tests/Functional/CdrTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Kazoo\Tests\Functional;
+
+use \Kazoo\Tests\Common\FunctionalTest;
+
+/**
+ * @group functional
+ */
+class CdrTest extends FunctionalTest
+{
+    /**
+     * @test
+     */
+    public function testGetCdrs() {
+        $cdrs = $this->getSDK()->Account()->Cdrs()->fetch();
+        $this->assertInstanceOf("\\Kazoo\\Api\\Collection\\Cdrs", $cdrs);
+        if (count($cdrs) > 0) {
+            $this->additonalTest($cdrs);
+        }
+    }
+
+    public function additonalTest($cdrs) {
+        $inbound_filter = array('filter_call_direction' => 'inbound');
+        $inbound_cdrs = $this->getSDK()->Account()->Cdrs($inbound_filter);
+
+        $outbound_filter = array('filter_call_direction' => 'outbound');
+        $outbound_cdrs = $this->getSDK()->Account()->Cdrs($outbound_filter);
+
+
+        $this->assertTrue(count($inbound_cdrs) != count($outbound_cdrs));
+        $this->assertTrue((count($inbound_cdrs) + count($outbound_cdrs)) == count($cdrs));
+
+        $cdr = current($cdrs)->fetch();
+
+        $this->assertInstanceOf("\\Kazoo\\Api\\Entity\\Cdr", $cdr);
+    }
+}
