393be9b2f85407b639fc9560efb071c0f1a34b83
karl anderson <karl@2600hz.com>
2015-01-30 18:59:49 -0500

    update composer binary

--- src/Kazoo/Api/Collection/PhoneNumbers.php
+++ src/Kazoo/Api/Collection/PhoneNumbers.php
@@ -2,23 +2,78 @@
 
 namespace Kazoo\Api\Collection;
 
+use \stdClass;
+
 class PhoneNumbers extends AbstractCollection
 {
+    /**
+     *
+     *
+     */
     public function classifiers() {
         $response = $this->get(array(), '/classifiers');
         $this->setCollection($response->getData());
+
         $element_wrapper = $this->getElementWrapper();
         $element_wrapper->unfetchable();
+
         return $this;
     }
 
-    public function find() {
+    /**
+     *
+     *
+     */
+    public function find($prefix, $quantity = 1) {
+        $sdk = $this->getSDK();
+        $this->setChain($sdk);
+
+        $filters = array(
+            'prefix' => $prefix,
+            'quantity' => (int)$quantity
+        );
+        $response = $this->get($filters);
+        $this->setCollection($response->getData());
+
+        return $this;
     }
 
-    public function locality() {
+    /**
+     *
+     *
+     */
+    public function locality(array $numbers, $quality = "standard") {
+        $payload = new stdClass;
+        $payload->data = new stdClass;
+        $payload->data->numbers = $numbers;
+        $payload->data->quality = $quality;
+
+        $response = $this->post(json_encode($payload), '/locality');
+        $this->setCollection($response->getData());
+
+        $element_wrapper = $this->getElementWrapper();
+        $element_wrapper->unfetchable();
+
+        return $this;
     }
 
+    /**
+     *
+     *
+     */
     public function check() {
+        $payload = new stdClass;
+        $payload->data = new stdClass;
+        $payload->data->numbers = $numbers;
+        $payload->data->quality = $quality;
+
+        $response = $this->post(json_encode($payload), '/check');
+        $this->setCollection($response->getData());
+
+        $element_wrapper = $this->getElementWrapper();
+        $element_wrapper->unfetchable();
+
+        return $this;
     }
 
     /**
--- src/Kazoo/Api/Entity/PhoneNumber.php
+++ src/Kazoo/Api/Entity/PhoneNumber.php
@@ -11,27 +11,47 @@ class PhoneNumber extends AbstractEntity
 {
     private $new_number = FALSE;
 
+    /**
+     *
+     *
+     */
     public function getId() {
         $id = parent::getId();
         return urlencode($id);
     }
 
+    /**
+     *
+     *
+     */
     public function activate() {
         $id = $this->getId();
         $this->setTokenValue($this->getEntityIdName(), $id);
         $this->put(array(), "/activate");
     }
 
+    /**
+     *
+     *
+     */
     public function reserve() {
         $id = $this->getId();
         $this->setTokenValue($this->getEntityIdName(), $id);
         $this->put(array(), "/reserve");
     }
 
+    /**
+     *
+     *
+     */
     public function port() {
         return $this->save('/port');
     }
 
+    /**
+     *
+     *
+     */
     public function identify() {
         $this->fetch('/identify');
         $this->readOnly();
@@ -39,32 +59,26 @@ class PhoneNumber extends AbstractEntity
     }
 
     /**
-     * Explicitly fetch from Kazoo, typicall it lazy-loads.
-     * This could also be used to re-load to ensure the data
-     * is fresh.
+     *
      *
      */
     public function fetch($append_uri = null) {
         $id = $this->getId();
-        $this->setTokenValue($this->getEntityIdName(), $id);
 
         try {
-            $response = $this->get(array(), $append_uri);
-            $entity = $response->getData();
+            parent::fetch($append_uri);
         } catch (ServerErrorResponseException $e) {
-            $entity = new stdClass;
+            $this->setEntity(new stdClass);
             $this->new_number = TRUE;
         }
 
-        $this->setEntity($entity);
         $this->setId($id);
 
         return $this;
     }
 
     /**
-     * Saves the current entity, if it does not have an
-     * id then it will be created.
+     *
      *
      */
     public function save($append_uri = null) {
