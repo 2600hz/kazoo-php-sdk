ea9181e9cc58e9536fab917c98c065a5078b1631
swysor <sean@2600hz.com>
2015-02-09 19:48:01 -0800

    syntax!

--- src/CallflowBuilder/Builder.php
+++ src/CallflowBuilder/Builder.php
@@ -11,6 +11,7 @@ class Builder {
     public function __construct(array $numbers = array(), array $patterns = array()){
        if (isset($numbers)){
             $this->numbers = $numbers;
+       }
        if (isset($patterns){ 
             $this->patterns = $patterns;
        } 
