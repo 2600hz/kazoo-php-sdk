9d0d58a231a7e30e180973097485af8e0d760de3
swysor <sean@2600hz.com>
2015-02-11 11:42:04 -0800

    fixed call flows so reverting builder

--- lib/hz2600/CallflowBuilder/Builder.php
+++ lib/hz2600/CallflowBuilder/Builder.php
@@ -5,9 +5,23 @@ namespace CallflowBuilder;
 use CallflowBuilder\Node\AbstractNode; 
 
 class Builder {
-    
-    public static function build(AbstractNode $node){
-         return $node->build();
+
+    public function __construct(array $numbers = array(), array $patterns = array()){
+        if (isset($numbers)){
+            $this->numbers = $numbers;
+        }
+        if (isset($patterns)){ 
+            $this->patterns = $patterns;
+        } 
+    }
+
+    public function build(AbstractNode $node){
+        $this->flow = $node->build();  
+        return $this; 
+    }
+   
+    public function __toString() {
+        return json_encode($this);
     }
 
 }
