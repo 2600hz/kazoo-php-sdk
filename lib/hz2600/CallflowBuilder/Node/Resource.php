c9280c0521c884cce90a668d4ab5223366fde8ad
swysor <sean@2600hz.com>
2015-02-11 16:10:20 -0800

    Resource now supports offnet and account resources

--- lib/hz2600/CallflowBuilder/Node/Resource.php
+++ lib/hz2600/CallflowBuilder/Node/Resource.php
@@ -2,11 +2,87 @@
 
 namespace CallflowBuilder\Node; 
 
-class Resource extends AbstractNode
+class Resources extends AbstractNode
 {
-    public function __construct($id) {
+    public function __construct($id = NULL) {
         parent::__construct();
         $this->module = "resources";
-        $this->data->hunt_account_id = $id;   
-   }   
+        if (isset($id)){
+           $this->huntAccountId($id);
+        }
+        else {
+           $this->useLocalResources(FALSE);
+        }   
+    }
+
+    public function toDid($did){
+        $this->data->to_did = $did;
+        return $this;  
+    }
+
+    public function media($media_id){
+        $this->data->media = $media_id; 
+        return $this; 
+    }
+  
+    public function ringback($ringback_id){
+        $this->data->ringback = $ringback_id;
+        return $this 
+    } 
+
+    public function formatFromDid($value){
+        $this->data->format_from_did = $value;
+        return $this;  
+    }
+
+    public function timeout($value){
+        $this->data->timeout = $value; 
+        return $this; 
+    }
+
+    public function doNotNormalize($value){
+        $this->data->do_not_normalize = $value; 
+        return $this; 
+    }
+
+    public function bypassE164($value){
+        $this->data->bypass_e164 = $value; 
+        return $this; 
+    }
+
+    public function fromUriRealm($realm){
+        $this->data->from_uri_realm = $realm;
+        return $this; 
+    }
+
+    public function callerIdType($type){
+        $this->data->caller_id_type = $type;
+        return $this; 
+    }
+
+    public function useLocalResources($value){
+        $this->data->use_local_resources = $value;
+        return $this; 
+    }
+ 
+    public function huntAccountId($id){
+        $this->data->hunt_account_id = $id; 
+        return $this; 
+    }
+
+    public function emitAccountId($id){
+        $this->data->emit_account_id = $id; 
+        return $this; 
+    }
+
+    public function customSipHeaders(array $headers){
+        $this->data->custom_sip_headers = $headers;
+        return $this; 
+    }   
+
+    public function ignoreEarlyMedia($value){
+        $this->data->ignore_early_media = $value; 
+        return $this; 
+    }   
+
 }
