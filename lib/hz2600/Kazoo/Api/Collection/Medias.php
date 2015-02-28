cb8caf63f28117337bd1ede7919c6e8c0f43a358
Blake McSwain <blake@collecttix.com>
2015-02-28 00:21:03 +0000

    Added media API

---
+++ lib/hz2600/Kazoo/Api/Collection/Medias.php
@@ -0,0 +1,12 @@
+<?php
+namespace Kazoo\Api\Collection;
+class Medias extends AbstractCollection
+{
+    /**
+     *
+     *
+     */
+    protected function getUriSnippet() {
+        return '/media';
+    }
+}
