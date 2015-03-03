dabec1fb5b12ba37886a9d05b3a86534945f93a1
Pershin Vladimir <pershin.v@sibtelcom.ru>
2015-03-03 17:30:25 +0600

    Add Carrier ACL for SystemConfig

---
+++ lib/hz2600/Kazoo/Api/Entity/SystemConfig.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Kazoo\Api\Entity;
+
+class SystemConfig extends AbstractEntity
+{
+    public function save($append_uri = null) {
+        $id = $this->getId();
+        $payload = $this->getPayload();
+        $this->setTokenValue($this->getEntityIdName(), $id);
+        $response = $this->post($payload, $append_uri);
+
+        $entity = $response->getData();
+        $this->setEntity($entity);
+
+        return $this;
+    }   
+
+    public function fetchAcls() {
+        $id = $this->getId();
+        $this->setTokenValue($this->getEntityIdName(), $id);
+        $response = $this->get(array(),"ecallmgr");
+        $entity=$response->getData();
+        $this->setEntity($entity);
+        return $this;
+    }       
+}
