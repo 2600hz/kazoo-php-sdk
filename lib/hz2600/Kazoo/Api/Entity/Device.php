115d6b35e8305f866aa736e74230fb082a237122
Chris Edwards <chris@email.co.ca>
2015-04-29 11:38:19 +0200

    Oops-- was going at this backwards.

--- lib/hz2600/Kazoo/Api/Entity/Device.php
+++ lib/hz2600/Kazoo/Api/Entity/Device.php
@@ -7,7 +7,7 @@ class Device extends AbstractEntity
     public function quickcall($number, $auto_answer = true) {
         $this->setTokenValue($this->getEntityIdName(), $this->getId());
         $this->setTokenValue('quickcall_number', $number);
-        $this->setTokenValue('auto_answer', ($auto_answer === 'true'));
+        $this->setTokenValue('auto_answer', $auto_answer ? 'true' : 'false');
         $this->get(array(), '/quickcall/{quickcall_number}?auto_answer={auto_answer}');
     }
 }
