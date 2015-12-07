af5d4090e9f7af5b0b7f9bbaa01ec9768e6f3a8c
kevin <kevinlovesing@gmail.com>
2015-12-07 09:00:05 +0000

    change  directory name from test to demo

---
+++ demo/test_connective.php
@@ -0,0 +1,25 @@
+<?php
+
+/* Install the library via composer or download the .zip file to your project folder. */
+/* This line loads the library */
+require_once "vendor/autoload.php";
+
+$device_id = '5a151492a3d46a34d0b8992f3647113a';
+
+
+/* Setup your SDK options, most commonly the Kazoo URL. If not provided defaults to localhost */
+$options = array('base_url' => 'http://192.168.181.25:8000');
+
+/* Get an authentication token */
+$authToken = new \Kazoo\AuthToken\User('admin', 'password', 'sip.realm.com');
+/* Create a new Kazoo SDK object */
+$sdk = new \Kazoo\SDK($authToken, $options);
+
+
+/* For the next examples, use a devices variable for convenience */
+$conns = $sdk->Account()->Connectivities();
+
+echo $conns;
+
+
+?>
