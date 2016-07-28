b2c36c227e2cfbd2f7ca882cf4885f5b39e40926
swysor <sean@2600hz.com>
2016-07-28 21:23:52 +0000

    Added apps link API to supported APIs

---
+++ lib/hz2600/Kazoo/Tests/Functional/AppsLinkTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Kazoo\Tests\Functional;
+
+use \Kazoo\Tests\Common\FunctionalTest;
+
+/**
+ * @group functional
+ */
+class AppsLinkTest extends FunctionalTest
+{
+    /**
+     * @test
+     */
+    public function testFetchLegitAuthToken() {
+        $apps_link = $this->getSDK()->Account()->AppsLink();
+        $this->assertInstanceOf("\\Kazoo\\Api\\Entity\\AppsLink", $apps_link);
+        $apps_link->fetch();
+        $this->assertTrue(is_object($apps_link->auth_token));
+        $this->assertTrue(is_object($apps_link->account));
+    }
+}
