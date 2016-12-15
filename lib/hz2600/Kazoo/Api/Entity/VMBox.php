cb1ae3a8c275711cab92232442d9f66e476a6238
kenrowland <kenrowland@mindless.com>
2016-12-14 20:18:12 -0500

    Add vmbox entity id token
    Since Messages are derived from a vmbox, the vmbox entitiy needs to have
    its entity id properly set.

--- lib/hz2600/Kazoo/Api/Entity/VMBox.php
+++ lib/hz2600/Kazoo/Api/Entity/VMBox.php
@@ -2,7 +2,12 @@
 
 namespace Kazoo\Api\Entity;
 
+use \Kazoo\Common\ChainableInterface;
+
 class VMBox extends AbstractEntity
 {
-
+    public function __construct(ChainableInterface $chain, array $arguments = array()) {
+        parent::__construct($chain, $arguments);
+        $this->setTokenValue($this->getEntityIdName(), $this->getId());
+    }
 }
