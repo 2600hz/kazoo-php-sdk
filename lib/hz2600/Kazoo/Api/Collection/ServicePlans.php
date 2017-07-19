196ec491ef5c2bb88c536555d254758985d106c6
Bret Truchan <bret@2600hz.com>
2017-07-19 15:09:32 -0700

    Added test units for service plans

--- lib/hz2600/Kazoo/Api/Collection/ServicePlans.php
+++ lib/hz2600/Kazoo/Api/Collection/ServicePlans.php
@@ -4,5 +4,9 @@ namespace Kazoo\Api\Collection;
 
 class ServicePlans extends AbstractCollection
 {
-
+    public function available() {
+        $response = $this->get(array(),'/available');
+        $this->setCollection($response->getData());
+        return $this;
+    }
 }
