ddd8fd900e6faf585f05a49cb8b4bfafecc24b49
Daniel Finke <danielfinke2011@gmail.com>
2017-12-07 11:57:13 -0800

    KAZPHP-4: add support for the device actions api (#112)

--- lib/hz2600/Kazoo/Api/Entity/Device.php
+++ lib/hz2600/Kazoo/Api/Entity/Device.php
@@ -4,6 +4,24 @@ namespace Kazoo\Api\Entity;
 
 class Device extends AbstractEntity
 {
+    /**
+     * Perform an action on this device
+     *
+     * @param string $action The name of the action to perform
+     * @param stdClass|null $data Optional extra data to control how the action
+     * is performed
+     */
+    public function performAction($action, $data = null) {
+        $this->setTokenValue($this->getEntityIdName(), $this->getId());
+
+        $payload = new \stdClass;
+        $payload->data = new \stdClass;
+        $payload->data->action = $action;
+        $payload->data->data = is_null($data) ? new \stdClass : $data;
+
+        $this->put(json_encode($payload));
+    }
+
     public function quickcall($number, $options = array()) {
         $url = '/quickcall/{quickcall_number}';
         $this->setTokenValue($this->getEntityIdName(), $this->getId());
