ae7c0d49528afdb62eb74b1149dd8a2dc39a0e60
powerpbx <admin@powerpbx.org>
2017-12-18 16:55:43 -0800

    Update VMBoxes.php

--- lib/hz2600/Kazoo/Api/Collection/VMBoxes.php
+++ lib/hz2600/Kazoo/Api/Collection/VMBoxes.php
@@ -4,5 +4,9 @@ namespace Kazoo\Api\Collection;
 
 class VMBoxes extends AbstractCollection
 {
-
+    public function messages() {
+        $response = $this->get(array(), '/messages');
+        $this->setCollection($response->getData());
+        return $this;
+    }
 }
