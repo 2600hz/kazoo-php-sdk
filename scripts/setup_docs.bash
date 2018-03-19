5dc1e00c18e30a0eb46dddc285dc39425730dc67
James Aimonetti <james.aimonetti@gmail.com>
2018-03-19 23:48:41 +0000

    Add circleci script and some basic targets to start
    Format PHP/JSON files
    
    Build a docs site from markdown to make sure docs are in mkdocs index
    and no obvious markdown formatting issues.

---
+++ scripts/setup_docs.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+## sets up the docs environment to serve mkdocs-built site
+
+pushd $(dirname $0) > /dev/null
+
+cd $(pwd -P)/..
+DOCS_ROOT=`cd ./docs/mkdocs && pwd -P`
+
+if [ -z `command -v cpio` ] ; then
+    echo "cpio command is not available, please install it" && popd > /dev/null && exit 1
+fi
+
+find . \( -path "./docs/mkdocs" -o -path './vendor' \) -prune -o -type f -regex ".+\.\(md\|png\|jpg\|svg|json\)$" -print | cpio -p -dum --quiet $DOCS_ROOT/docs
+
+popd > /dev/null
