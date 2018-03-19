15bf876b4519051dd1607db2007e89bd11159494
James Aimonetti <james.aimonetti@gmail.com>
2018-03-19 23:23:06 +0000

    add start of conference dial

--- lib/hz2600/Kazoo/Api/Entity/Conference.php
+++ lib/hz2600/Kazoo/Api/Entity/Conference.php
@@ -5,4 +5,31 @@ namespace Kazoo\Api\Entity;
 
 class Conference extends AbstractEntity
 {
-}    
+    public function dial($data) {
+        $envelope = new stdClass();
+        $envelope->action = "dial";
+        $envelope->data = $data;
+
+        $id = $this->getId();
+
+        $this->execute($envelope)
+    }
+
+    public function execute($envelope) {
+        if ($this->read_only) {
+            throw new ReadOnly("The entity is read-only");
+        }
+
+        $id = $this->getId();
+        $payload = json_encode($envelope);
+
+        $this->setTokenValue($this->getEntityIdName(), $id);
+
+        $response = $this->put($payload);
+
+        $entity = $response->getData();
+        $this->setEntity($entity);
+
+        return $this;
+    }
+}
