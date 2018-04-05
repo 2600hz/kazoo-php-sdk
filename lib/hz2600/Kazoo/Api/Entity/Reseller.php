f468124f5ad35d7588be5c8fa19bdfdc0e775c18
plau <plau256@gmail.com>
2018-04-04 18:27:19 -0700

    adds ability to promote account to reseller

---
+++ lib/hz2600/Kazoo/Api/Entity/Reseller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Kazoo\Api\Entity;
+
+use \stdClass;
+
+use \Exception;
+
+use \Kazoo\Common\ChainableInterface;
+use \Kazoo\Api\Collection\Resellers;
+
+class Reseller extends AbstractEntity
+{
+
+}
